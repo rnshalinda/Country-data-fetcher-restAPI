409887b06574de9b1c406a27227617252f91cc6d
rnshalinda <nsrhewa@gmail.com>
2025-07-10 09:45:57 +0530

    small adjustments color, border, box sizes

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-
-// fetch("https://jsonplaceholder.typicode.com/todos").then(res => res.json()).then(data => {
-//     console.log(data);
-
-//     data.forEach(element => {
-//         document.writeln(element.id)
-//     });
-    
-// })
-
-
 function btnAction(){
     let txt = document.getElementById("userInput").value
-    let heading1 = document.getElementById("countryName")
-    let heading2 = document.getElementById("nativeName")
+    let countryName = document.getElementById("countryName")
+    let officialName = document.getElementById("nativeName")
     let countryFlag = document.getElementById("flag")
     let capital = document.getElementById("capital")
     let language = document.getElementById("lang")
@@ -25,19 +14,19 @@ function btnAction(){
     let latlng = document.getElementById("latlng")
     let map = document.getElementById("map")
 
-
-    
+    // API
     fetch("https://restcountries.com/v3.1/name/"+txt).then(res => res.json()).then(data => {
-        console.log(data);
-        
-        heading1.innerText = data[0].name.common
+        console.log(data); // for debugging
+        // country name headings
+        countryName.innerText = data[0].name.common
 
         if(data[0].name.official){
-            heading2.innerText = data[0].name.official
+            officialName.innerText = data[0].name.official
         }
-        else{heading2.innerText = "Official name not available"}
+        else{officialName.innerText = "Official name not available"}
         
-        countryFlag.innerHTML = `<img src="${data[0].flags.png}" alt="Country Flag" width="200"></img>`
+        // flag
+        countryFlag.innerHTML = `<img src="${data[0].flags.png}" alt="Country Flag" width="180"></img>`
 
         // region
         region.innerText = data[0].region
@@ -78,10 +67,11 @@ function btnAction(){
         // timezones
         timezones.innerText = data[0].timezones
 
+        // geo location
         let lat = data[0].capitalInfo.latlng[0]
         let lng = data[0].capitalInfo.latlng[1]
         latlng.innerText = `Geo Coordinates (Latitude ${lat}° N, Longitude ${lng}° E)`
-    
+        // map preview
         map.innerHTML = `<iframe src="https://www.google.com/maps?q=${lat},${lng}&hl=en&z=5&output=embed" frameborder="0"></iframe>`
     })
 }
